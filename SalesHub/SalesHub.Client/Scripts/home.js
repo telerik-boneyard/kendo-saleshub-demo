32b60db6a9881f8c2c317aa075e13cf99afa17f9
Thomas Mullaly <tmullaly88@gmail.com>
2013-05-22 09:42:51 -0400

    Use the Select event from the Kendo Treeview extensions

--- SalesHub/SalesHub.Client/Scripts/home.js
+++ SalesHub/SalesHub.Client/Scripts/home.js
@@ -1,18 +1,12 @@
-﻿$(document).ready(function () {
+﻿(function () {
     'use strict';
 
-    var selectedCustomerLi, treeView;
-    var headerViewModel = window.SalesHub.viewModels.headerViewModel;
-    var queryStringParams;
-
-    kendo.bind($("header"), headerViewModel);
-
-    var getUrlParams = function() {
+    var getUrlParams = function () {
         var urlParams = {};
         var match,
             pl = /\+/g, // Regex for replacing addition symbol with a space
             search = /([^&=]+)=?([^&]*)/g,
-            decode = function(s) { return decodeURIComponent(s.replace(pl, " ")); },
+            decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
             query = window.location.search.substring(1);
 
         while (match = search.exec(query)) {
@@ -21,21 +15,12 @@
         return urlParams;
     };
 
-    queryStringParams = getUrlParams();
-
-    if ("customerId" in queryStringParams) {
-        selectedCustomerLi = $("#customerTreeView li[data-customer-id=" + queryStringParams['customerId'] + "]");
-    } else {
-        selectedCustomerLi = $("#customerTreeView li[data-customer-id]:first");
-    }
-    treeView = $("#customerTreeView").data("kendoTreeView");
-
-    var updateGridCustomerFilter = function(customerId) {
+    var updateGridCustomerFilter = function (customerId) {
         var ordersGrid = $("#ordersGrid").data("kendoGrid");
         ordersGrid.dataSource.filter({ field: "CustomerId", operator: "eq", value: customerId });
     };
 
-    treeView.bind("select", function(e) {
+    window.SalesHub.CustomerTreeView_Select = function (e) {
         var node = $(e.node);
         var dataItem = e.sender.dataItem(e.node);
 
@@ -43,13 +28,30 @@
             window.SalesHub.viewModels.headerViewModel.updateSelectedCustomer(node.data("customer-id"), node.data("path"));
             updateGridCustomerFilter(node.data("customer-id"));
         }
-    });
+    };
 
-    $("#createOrderButton").on("click", function () {
-        window.location.href = window.SalesHub.createOrderUrl + '/' + headerViewModel.selectedCustomerId;
+    $(document).ready(function() {
+        var selectedCustomerLi, treeView;
+        var headerViewModel = window.SalesHub.viewModels.headerViewModel;
+        var queryStringParams;
+
+        kendo.bind($("header"), headerViewModel);
+
+        queryStringParams = getUrlParams();
+
+        if ("customerId" in queryStringParams) {
+            selectedCustomerLi = $("#customerTreeView li[data-customer-id=" + queryStringParams['customerId'] + "]");
+        } else {
+            selectedCustomerLi = $("#customerTreeView li[data-customer-id]:first");
+        }
+        treeView = $("#customerTreeView").data("kendoTreeView");
+
+        $("#createOrderButton").on("click", function() {
+            window.location.href = window.SalesHub.createOrderUrl + '/' + headerViewModel.selectedCustomerId;
+        });
+        treeView.select(selectedCustomerLi);
+        treeView.expand(selectedCustomerLi.parent());
+        headerViewModel.updateSelectedCustomer(selectedCustomerLi.data("customer-id"), selectedCustomerLi.data("path"));
+        updateGridCustomerFilter(selectedCustomerLi.data("customer-id"));
     });
-    treeView.select(selectedCustomerLi);
-    treeView.expand(selectedCustomerLi.parent());
-    headerViewModel.updateSelectedCustomer(selectedCustomerLi.data("customer-id"), selectedCustomerLi.data("path"));
-    updateGridCustomerFilter(selectedCustomerLi.data("customer-id"));
-});
+})();
