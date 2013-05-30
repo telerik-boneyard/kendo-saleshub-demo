e1914914700db17343e1c1c25f4bbd699b64bc66
Thomas Mullaly <tmullaly88@gmail.com>
2013-05-30 10:08:20 -0400

    Added a new change event handler to avoid confusion

--- SalesHub/SalesHub.Client/Scripts/order.js
+++ SalesHub/SalesHub.Client/Scripts/order.js
@@ -1,60 +1,70 @@
-﻿window.SalesHub.OrderDetailsGrid_Save = function(e) {
-    e.model.set("TotalAmount", e.model.Units * e.model.PricePerUnitOfWeight);
-};
+﻿(function () {
+    'use strict';
 
-window.SalesHub.OrderDetailsEdit_NetWeight_Change = function (e) {
-    var grid = $("#orderDetailsGrid").data("kendoGrid");
-    var orderDetails = grid.editable.options.model;
-    if (orderDetails.UnitWeight !== 0) {
-        orderDetails.set("Units", orderDetails.NetWeight / orderDetails.UnitWeight);
-    }
-};
+    window.SalesHub.OrderDetailsGrid_Save = function(e) {
+        e.model.set("TotalAmount", e.model.Units * e.model.PricePerUnitOfWeight);
+    };
 
-window.SalesHub.OrderDetailsEdit_UnitWeight_Change = function (e) {
-    var grid = $("#orderDetailsGrid").data("kendoGrid");
-    var orderDetails = grid.editable.options.model;
-    orderDetails.set("NetWeight", orderDetails.UnitWeight * orderDetails.Units);
-};
-
-window.SalesHub.OrderDetails_Error = function (args) {
-    if (args.errors) {
+    window.SalesHub.OrderDetailsEdit_NetWeight_Change = function(e) {
         var grid = $("#orderDetailsGrid").data("kendoGrid");
-        var validationTemplate = kendo.template($("#orderDetailsValidationMessageTemplate").html());
-        grid.one("dataBinding", function (e) {
-            e.preventDefault();
+        var orderDetails = grid.editable.options.model;
+        if (orderDetails.UnitWeight !== 0) {
+            orderDetails.set("Units", orderDetails.NetWeight / orderDetails.UnitWeight);
+        }
+    };
 
-            $.each(args.errors, function (propertyName) {
-                var renderedTemplate = validationTemplate({ field: propertyName, messages: this.errors });
-                grid.editable.element.find(".errors").append(renderedTemplate);
+    window.SalesHub.OrderDetailsEdit_UnitWeight_Change = function(e) {
+        var grid = $("#orderDetailsGrid").data("kendoGrid");
+        var orderDetails = grid.editable.options.model;
+        orderDetails.set("NetWeight", orderDetails.UnitWeight * orderDetails.Units);
+    };
+
+    window.SalesHub.OrderDetailsEdit_Units_Change = function() {
+        var grid = $("#orderDetailsGrid").data("kendoGrid");
+        var orderDetails = grid.editable.options.model;
+        orderDetails.set("NetWeight", orderDetails.UnitWeight * orderDetails.Units);
+    };
+
+    window.SalesHub.OrderDetails_Error = function(args) {
+        if (args.errors) {
+            var grid = $("#orderDetailsGrid").data("kendoGrid");
+            var validationTemplate = kendo.template($("#orderDetailsValidationMessageTemplate").html());
+            grid.one("dataBinding", function(e) {
+                e.preventDefault();
+
+                $.each(args.errors, function(propertyName) {
+                    var renderedTemplate = validationTemplate({ field: propertyName, messages: this.errors });
+                    grid.editable.element.find(".errors").append(renderedTemplate);
+                });
             });
+        }
+    };
+
+    window.SalesHub.SuggestedValuesGrid_Changed = function(e) {
+        var selectedRow = this.select()[0];
+
+        $("#PaymentTermsOverride").val($(selectedRow).find("td").text());
+        $("#suggestedValuesWindow").data("kendoWindow").close();
+    };
+
+    $(document).ready(function() {
+        var customerData = window.SalesHub.customerData;
+
+        window.SalesHub.setCustomerPath(customerData.customerPath);
+
+        $(".actionButton").on("click", function(e) {
+            window.location.href = $(e.target).data("action");
         });
-    }
-};
 
-window.SalesHub.SuggestedValuesGrid_Changed = function (e) {
-    var selectedRow = this.select()[0];
+        $("#delete").on("click", function() {
+            $("#deleteForm").submit();
+        });
 
-    $("#PaymentTermsOverride").val($(selectedRow).find("td").text());
-    $("#suggestedValuesWindow").data("kendoWindow").close();
-};
+        $("#suggestedValueButton").on("click", function(e) {
+            var suggestedValuesWindow = $("#suggestedValuesWindow").data("kendoWindow");
 
-$(document).ready(function () {
-    var customerData = window.SalesHub.customerData;
-
-    window.SalesHub.setCustomerPath(customerData.customerPath);
-
-    $(".actionButton").on("click", function (e) {
-        window.location.href = $(e.target).data("action");
+            suggestedValuesWindow.center();
+            suggestedValuesWindow.open();
+        });
     });
-
-    $("#delete").on("click", function() {
-        $("#deleteForm").submit();
-    });
-
-    $("#suggestedValueButton").on("click", function (e) {
-        var suggestedValuesWindow = $("#suggestedValuesWindow").data("kendoWindow");
-
-        suggestedValuesWindow.center();
-        suggestedValuesWindow.open();
-    });
-});
+})();
