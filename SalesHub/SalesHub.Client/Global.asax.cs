cedddd123e8ea48ff091525d29c83f50652ff5da
Thomas Mullaly <tmullaly88@gmail.com>
2013-06-03 18:33:24 -0400

    Recreate database every 24 hours. Reduced number of customers and saves to the database during creation

--- SalesHub/SalesHub.Client/Global.asax.cs
+++ SalesHub/SalesHub.Client/Global.asax.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Data.Entity;
 using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Caching;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -12,10 +15,15 @@ namespace SalesHub.Client
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
+        private static DateTime _lastMaintenanceTime;
+        private static bool _isInMaintenanceMode;
+
         protected void Application_Start()
         {
+            _lastMaintenanceTime = DateTime.Now;
+
             Database.SetInitializer<SalesHubDbContext>(new SalesHubDbInitializer());
 
             AreaRegistration.RegisterAllAreas();
@@ -24,6 +32,31 @@ namespace SalesHub.Client
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (_isInMaintenanceMode && Request.Url.AbsolutePath != "/Maintenance")
+            {
+                Response.Redirect("~/Maintenance", true);
+            }
+
+            DateTime now = DateTime.Now;
+            if (now > _lastMaintenanceTime.AddHours(24))
+            {
+                _lastMaintenanceTime = now;
+                _isInMaintenanceMode = true;
+
+                Response.Redirect("~/Maintenance", false);
+                Response.Flush();
+
+                var dbContext = new SalesHubDbContext();
+                dbContext.Database.Delete();
+                var initializer = new SalesHubDbInitializer();
+                initializer.InitializeDatabase(dbContext);
+
+                _isInMaintenanceMode = false;
+            }
         }
     }
 }
--- SalesHub/SalesHub.Data/SalesHubDbInitializer.cs
+++ SalesHub/SalesHub.Data/SalesHubDbInitializer.cs
@@ -6,7 +6,7 @@ using SalesHub.Core.Models;
 
 namespace SalesHub.Data
 {
-    public class SalesHubDbInitializer : DropCreateDatabaseIfModelChanges<SalesHubDbContext>
+    public class SalesHubDbInitializer : DropCreateDatabaseAlways<SalesHubDbContext>
     {
         private readonly Random _random = new Random();
 
@@ -73,70 +73,54 @@ namespace SalesHub.Data
         {
             string [] customerNames = new[]
                 {
-                "Alfreds Futterkiste",
-                "Ana Trujillo Emparedados y helados",
-                "Antonio Moreno Taquería",
                 "Around the Horn",
-                "Berglunds snabbköp",
                 "Blauer See Delikatessen",
-                "Blondesddsl père et fils",
-                "Bólido Comidas preparadas",
-                "Bon app'",
                 "Bottom-Dollar Markets",
-                "B's Beverages",
-                "Cactus Comidas para llevar",
-                "Centro comercial Moctezuma",
                 "Chop-suey Chinese",
-                "Comércio Mineiro",
-                "Consolidated Holdings",
-                "Die Wandernde Kuh",
-                "Drachenblut Delikatessen",
-                "Du monde entier",
                 "Eastern Connection",
-                "Folk och fä HB",
-                "France restauration",
-                "Franchi S.p.A.",
-                "Frankenversand",
-                "Furia Bacalhau e Frutos do Mar",
-                "alería del gastrónomo",
                 "Godos Cocina Típica",
                 "Lazy K Kountry Store",
-                "Lehmanns Marktstand",
                 "Let's Stop N Shop",
-                "LILA-Supermercado",
-                "LINO-Delicateses",
-                "Richter Supermarkt",
-                "Romero y tomillo",
                 "Santé Gourmet",
                 "Save-a-lot Markets",
                 "Suprêmes délices",
                 "The Big Cheese",
-                "The Cracker Box",
-                "Victuailles en stock",
-                "Vins et alcools Chevalier",
-                "Wartian Herkku",
-                "Wellington Importadora",
-                "White Clover Markets",
-                "Wilman Kala",
-                "Wolski  Zajazd"
+                "The Cracker Box"
             };
 
+            var customers = new List<Customer>();
             foreach (string customerName in customerNames)
             {
                 var customer = new Customer
-                    {
-                        CustomerName = customerName,
-                        SellingCompany = sellingCompany,
-                        Orders = new List<Order>()
-                    };
+                {
+                    CustomerName = customerName,
+                    SellingCompany = sellingCompany,
+                    Orders = new List<Order>()
+                };
 
+                customers.Add(customer);
                 context.Customers.Add(customer);
-                context.SaveChanges();
-                GenerateOrdersForCustomer(customer, context);
             }
+            context.SaveChanges();
+
+            var orders = new List<Order>();
+            foreach (var customer in customers)
+            {
+                orders.AddRange(GenerateOrdersForCustomer(customer, context));
+            }
+            context.SaveChanges();
+
+            foreach (var order in orders)
+            {
+                var orderDetails = GenerateOrderDetailsForOrder(order, context);
+
+                order.ContractAmount = orderDetails.Sum(od => od.PricePerUnitOfWeight * od.Units);
+                order.ContractWeight = orderDetails.Sum(od => od.Units * od.UnitWeight);
+            }
+            context.SaveChanges();
         }
 
-        private void GenerateOrdersForCustomer(Customer customer, SalesHubDbContext context)
+        private IEnumerable<Order> GenerateOrdersForCustomer(Customer customer, SalesHubDbContext context)
         {
             int orderCount = _random.Next(10, 20);
 
@@ -164,11 +148,8 @@ namespace SalesHub.Data
 
                 customer.Orders.Add(order);
                 context.Orders.Add(order);
-                context.SaveChanges();
-                var orderDetails = GenerateOrderDetailsForOrder(order, context);
 
-                order.ContractAmount = orderDetails.Sum(od => od.PricePerUnitOfWeight*od.Units);
-                order.ContractWeight = orderDetails.Sum(od => od.Units*od.UnitWeight);
+                yield return order;
             }
         }
 
@@ -179,18 +160,18 @@ namespace SalesHub.Data
             for (int i = 0; i < numToGenerate; i++)
             {
                 var orderDetail = new OrderDetail
-                    {
-                        PricePerUnitOfWeight = _random.Next(4, 100),
-                        CropYear = _random.Next(2002, 2013),
-                        Destination = "",
-                        LotNumber = "",
-                        OrderId = order.OrderId,
-                        Origin = "",
-                        PackageTypeId = _random.Next(1, context.PackageTypes.Count()),
-                        UnitWeight = _random.Next(10, 20),
-                        Units = _random.Next(2000, 6000),
-                        ValueDate = DateTime.Now.AddDays(_random.Next(2, 19))
-                    };
+                {
+                    PricePerUnitOfWeight = _random.Next(4, 100),
+                    CropYear = _random.Next(2002, 2013),
+                    Destination = "",
+                    LotNumber = "",
+                    OrderId = order.OrderId,
+                    Origin = "",
+                    PackageTypeId = _random.Next(1, context.PackageTypes.Count()),
+                    UnitWeight = _random.Next(10, 20),
+                    Units = _random.Next(2000, 6000),
+                    ValueDate = DateTime.Now.AddDays(_random.Next(2, 19))
+                };
                 orderDetail.NetWeight = orderDetail.UnitWeight * orderDetail.Units;
                 orderDetails.Add(orderDetail);
                 context.OrderDetails.Add(orderDetail);
